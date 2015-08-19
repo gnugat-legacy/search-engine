1c005465d8346c14028a2bdfb8ebe2395ba4c663
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-08-19 08:16:52 +0100

    Created Ordering

---
+++ spec/Gnugat/SearchEngine/Criteria/OrderingSpec.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the gnugat/search-engine package.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Gnugat\SearchEngine\Criteria;
+
+use PhpSpec\ObjectBehavior;
+
+class OrderingSpec extends ObjectBehavior
+{
+    const FIELD = 'field';
+    const DIRECTION = 'ASC';
+
+    function let()
+    {
+        $this->beConstructedWith(self::FIELD, self::DIRECTION);
+    }
+
+    function it_has_a_field()
+    {
+        $this->getField()->shouldBe(self::FIELD);
+    }
+
+    function it_has_a_direction()
+    {
+        $this->getDirection()->shouldBe(self::DIRECTION);
+    }
+}
