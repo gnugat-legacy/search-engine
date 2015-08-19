b5d3105de6b47e877d9fef41a9598dfeb0516fbd
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-08-19 19:07:49 +0100

    Created QueryBuilder

--- src/Gnugat/SearchEngine/Builder/OrderingsBuilder.php
+++ src/Gnugat/SearchEngine/Builder/OrderingsBuilder.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is part of the gnugat/search-engine package.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gnugat\SearchEngine\Builder;
 
 interface OrderingsBuilder
 {
-
     public function build($argument1, $argument2, $argument3);
 }
--- src/Gnugat/SearchEngine/QueryBuilder.php
+++ src/Gnugat/SearchEngine/QueryBuilder.php
@@ -1,13 +1,31 @@
 <?php
 
+/*
+ * This file is part of the gnugat/search-engine package.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gnugat\SearchEngine;
 
 interface QueryBuilder
 {
+    /**
+     * @param mixed $select A string (e.g. `COUNT(id) AS total`) or an array of fields (e.g. `['id', 'name']`)
+     */
+    public function select($select);
 
-    public function from($argument1);
+    /**
+     * @param string $from
+     * @param string $alias
+     */
+    public function from($resource, $alias = null);
 
-    public function select($argument1);
-
+    /**
+     * @return \Traversable Depending on the implementation, could be an array or an iterator.
+     */
     public function execute();
 }
